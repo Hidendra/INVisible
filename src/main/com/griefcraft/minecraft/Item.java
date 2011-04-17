53755d9bf49fbbd43fbfa3cc1b19b0b5c09b4bbf
Pavel Kuriscak <airguru@eastforce.net>
2011-04-17 09:41:39 +0200

    Added item names list
    The output is now more human-readable.

--- src/main/com/griefcraft/minecraft/Item.java
+++ src/main/com/griefcraft/minecraft/Item.java
@@ -17,8 +17,38 @@
 
 package com.griefcraft.minecraft;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Item {
 
+	/**
+	 * Item names for improved reading
+	 */
+        public static final Map<Integer, String> items;
+        static {
+            Map<Integer,String> itemMap = new HashMap<Integer,String>();
+
+            String[] block_names = {"air", "stone", "grass", "dirt", "cobblestone", "wood", "sapling", "bedrock", "water", "spring", "lava", "lava-spring", "sand", "gravel", "gold-ore", "iron-ore", "coal-ore", "log", "leaves", "sponge", "glass", "lapis-lazuli-ore", "lapis-lazuli", "dispenser", "sandstone", "note-block", "agua-cloth", "cyan-cloth", "blue-cloth", "purple-cloth", "indigo-cloth", "violet-cloth", "magenta-cloth", "pink-cloth", "black-cloth", "cloth", "wool", "flower", "rose", "brown-mushroom", "red-mushroom", "gold", "iron", "double-step", "step", "brick", "tnt", "bookshelf", "mossy-cobblestone", "obsidian", "torch", "fire", "mob-spawner", "wooden-stairs", "chest", "redstone-wire", "diamond-ore", "diamond", "workbench", "crops", "soil", "furnace", "burning-furnace", "signpost", "wooden-door", "ladder", "tracks", "stone-stairs", "wall-sign", "lever", "stone-plate", "iron-door", "wooden-plate", "redstone-ore", "glowing-redstone-ore", "redstone-torch-off", "redstone-torch", "stone-button", "snow", "ice", "snow-block", "cactus", "clay", "sugar-cane", "jukebox", "fence", "pumpkin", "brimstone", "slow-sand", "lightstone", "portal", "jack-o-lantern", "cake"};
+            String[] item_names = {"iron-shovel", "iron-pickaxe", "iron-axe", "flint-and-steel", "apple", "bow", "arrow", "coal", "diamond", "iron-ingot", "gold-ingot", "iron-sword", "wooden-sword", "wooden-shovel", "wooden-pickaxe", "wooden-axe", "stone-sword", "stone-shovel", "stone-pickaxe", "stone-axe", "diamond-sword", "diamond-shovel", "diamond-pickaxe", "diamond-axe", "stick", "bowl", "mushroom-soup", "gold-sword", "gold-shovel", "gold-pickaxe", "gold-axe", "string", "feather", "sulphur", "wooden-hoe", "stone-hoe", "iron-hoe", "diamond-hoe", "gold-hoe", "seeds", "wheat", "bread", "leather-helmet", "leather-chestplate", "leather-leggings", "leather-boots", "chainmail-helmet", "chainmail-chestplate", "chainmail-leggings", "chainmail-boots", "iron-helmet", "iron-chestplate", "iron-leggings", "iron-boots", "diamond-helmet", "diamond-chestplate", "diamond-leggings", "diamond-boots", "gold-helmet", "gold-chestplate", "gold-leggings", "gold-boots", "flint", "raw-porkchop", "cooked-porkchop", "paintings", "golden-apple", "sign", "wooden-door", "bucket", "water-bucket", "lava-bucket", "mine-cart", "saddle", "iron-door", "redstone", "snowball", "boat", "leather", "milk", "clay-brick", "clay-balls", "sugar-cane", "paper", "book", "slimeball", "storage-minecart", "powered-minecart", "egg", "compass", "fishing-rod", "clock", "glowstone-dust", "raw-fish", "cooked-fish", "dye", "bone", "sugar", "cake", "bed", "redstone-repeater", "cookie"};
+            String[] special_item_names = {"gold-music-disc", "green-music-disc"};
+
+            for (int i = 0;i<block_names.length;i++) {
+                itemMap.put(i, block_names[i]);
+            }
+
+            for (int i = 0;i<item_names.length;i++) {
+                itemMap.put(i+0x100, item_names[i]);
+            }
+
+            for (int i = 0;i<special_item_names.length;i++) {
+                itemMap.put(i+0x8d0, special_item_names[i]);
+            }
+
+            items = Collections.unmodifiableMap(itemMap);
+        }
+    
 	/**
 	 * Item slot
 	 */
@@ -73,7 +103,7 @@ public class Item {
 
 	@Override
 	public String toString() {
-		return String.format("%d\t[slot: %d]\t[amount: %d]\t[damage: %d]", id, slot, amount, damage);
+		return String.format("%s\t[slot: %d]\t[amount: %d]\t[damage: %d]", items.get(id), slot, amount, damage);
 	}
 
 }
